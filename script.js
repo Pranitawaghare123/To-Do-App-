3575fdaed1fbe83b906c573fe4f35474b20c55e2
Pranita Waghare <wagharepranita@gmail.com>
2026-05-18 02:19:35 +0530

    Enhanced todo app UI and added task creation form

---
+++ script.js
@@ -0,0 +1,8 @@
+const add_task_Button=document.querySelector(".add")
+
+// creating task
+add_task_Button.addEventListener("click",(e)=>{
+    console.log("add task is clicked")
+    let form=document.createElement("div")
+    div.classList.add("form")
+})
